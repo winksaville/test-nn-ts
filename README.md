1ccf2ae95e6de84799465498d6901f4eb881320f
Wink Saville <wink@saville.com>
2017-06-03 14:45:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# A simple artificial neural network library in TypeScript
+
+Based on my [test-nn](https://github.com/winksaville/test-nn) which is based
+on the [Step by Step Guide to Implementing a Neural Network in C](http://www.cs.bham.ac.uk/~jxb/INC/nn.html)
+by [John A. Bullinaria](http://www.cs.bham.ac.uk/~jxb/).
---
+++ test-nn.ts
@@ -0,0 +1,42 @@
+/*
+ * Copyright 2017 Wink Saville
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+import * as process from 'process';
+
+import * as debugModule from "debug";
+const dbg = debugModule("test-nn");
+
+function main(argv: string[]) {
+    try {
+        dbg("test-nn:+");
+
+        if (argv.length < 4) {
+            console.log(`Usage: yarn test <number of epochs> <output path>`);
+            argv.forEach((val, index) => {
+                console.log(`${index}: ${val}`);
+            });
+
+            return;
+        }
+        let epoch_count: number = parseInt(argv[2]);
+        let out_path: string = argv[3];
+
+        console.log(`epoch_count=${epoch_count} out_path=${out_path}`);
+    } finally {
+        dbg("test-nn:-");
+    }
+}
+
+main(process.argv);
